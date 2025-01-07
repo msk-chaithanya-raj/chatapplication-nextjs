f4f5d1e174c21c3108e85f991d2882f7dea55a05
Sri Krishna Chaithanya Raj Masimukku <mskchaithanyaraj@gmail.com>
2025-01-07 12:54:03 +0530

    chat app v1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import Button from "@/components/ui/Button";
+import { db } from "@/lib/db";
 import { FC } from "react";
 
-const Home: FC = ({}) => {
+const Home: FC = async ({}) => {
   return (
     <div>
       <Button>Hello</Button>
---
+++ src/types/db.d.ts
@@ -0,0 +1,6 @@
+interface User {
+  name: string;
+  email: string;
+  id: string;
+  image: string;
+}
